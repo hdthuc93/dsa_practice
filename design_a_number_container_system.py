5fdd4729676b6c703ad527824fd5095311414e8e
Thuc Huynh <hdthuc93@gmail.com>
2025-02-09 00:44:38 +0700

    save

---
+++ design_a_number_container_system.py
@@ -0,0 +1,42 @@
+# https://leetcode.com/problems/design-a-number-container-system/
+
+import heapq
+from collections import defaultdict
+
+
+class NumberContainers:
+
+    def __init__(self):
+        self.idx_num = {}
+        self.num_indices = defaultdict(list)
+
+    def change(self, index: int, number: int) -> None:
+        self.idx_num[index] = number
+        heapq.heappush(self.num_indices[number], index)
+
+    def find(self, number: int) -> int:
+        indices = self.num_indices.get(number, [])
+        while indices:
+            smallest_idx = heapq.heappop(indices)
+            if self.idx_num[smallest_idx] == number:
+                res = smallest_idx
+                heapq.heappush(indices, smallest_idx)
+                return smallest_idx
+
+        return -1
+
+
+# Your NumberContainers object will be instantiated and called as such:
+obj = NumberContainers()
+obj.change(75, 40)
+obj.change(27, 40)
+obj.change(22, 14)
+obj.change(85, 14)
+obj.change(22, 40)
+obj.change(18, 34)
+obj.change(92, 41)
+obj.change(22, 40)
+obj.change(75, 40)
+obj.change(59, 34)
+param_2 = obj.find(40)
+print(param_2)
